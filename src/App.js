b5e33acdfd3f0976ffc5644f1b9f838af8f0e63c
Ben Hencke <brainstar@gmail.com>
2018-08-11 18:03:24 -0700

    show controllers, formatting

--- src/App.js
+++ src/App.js
@@ -88,16 +88,33 @@ class App extends Component {
           </header>
 
           <main role="main">
-            <div className="jumbotron">
-              <h1 className="display-6">Here's what I found</h1>
-              <div className="list-group">
-                {this.state.groups.map(g =>
-                    <a key={g.name} href="#" className="list-group-item" onClick={(e) => this.handleGroupClick(e,g)}>
-                      <h2>{g.name}</h2>
-                      <small>{_.map(g.pixelblazes, 'name').join(', ')}</small>
-                    </a>
-                )}
+            <hr/>
+
+            <div className="row">
+              <div className="col-lg-12">
+                <h3>Controllers</h3>
+                <ul className="list-group col-lg-8" id="list">
+                  {this.state.discoveries.map(d =>
+                      <li className="list-group-item">
+                        <a className="btn btn-primary float-right" href={"http://" + d.address} target="_blank">Open</a>
+                        <h5>{d.name || "Pixelblaze_" + d.id} v{d.ver} @ {d.address}</h5>
+                      </li>
+                  )}
+                </ul>
               </div>
+            </div>
+            <hr/>
+
+            <h3>Patterns</h3>
+            <div className="list-group">
+              {this.state.groups.map(g =>
+                  <a key={g.name} href="#" className="list-group-item" onClick={(e) => this.handleGroupClick(e, g)}>
+                    <h5>{g.name}</h5>
+                    <em>
+                      <small>{_.map(g.pixelblazes, 'name').join(', ')}</small>
+                    </em>
+                  </a>
+              )}
             </div>
 
           </main>
